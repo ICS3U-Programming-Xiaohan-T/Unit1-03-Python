7b278e172faddf22e5635226e990a6cbf0b63699
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-02-13 03:12:33 +0000

    Added hello_world_style.py with proper formatting

---
+++ hello_world_style.py
@@ -0,0 +1,10 @@
+# Created by: Xiaohan T
+# Date: February 12,2025
+# This program prints "Hello, World!" with proper style.
+
+def main():
+    """Main function that prints Hello, World!"""
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
