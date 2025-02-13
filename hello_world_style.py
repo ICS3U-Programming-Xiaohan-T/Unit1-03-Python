9777dab898cc9ca7cdd7a59517c0c5929414c79e
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-02-13 03:19:27 +0000

    Formatted hello_world_style.py with black

--- hello_world_style.py
+++ hello_world_style.py
@@ -2,9 +2,11 @@
 # Date: February 12,2025
 # This program prints "Hello, World!" with proper style.
 
+
 def main():
     """Main function that prints Hello, World!"""
     print("Hello, World!")
+
 
 if __name__ == "__main__":
     main()
